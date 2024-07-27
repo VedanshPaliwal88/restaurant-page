c0e64363f45bc85c6213b9912038fe31ee553f68
VedanshPaliwal88 <66903973+VedanshPaliwal88@users.noreply.github.com>
2024-07-27 23:40:23 +0530

    added click event to open menu page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ('./css/style.css');
-import generateHome from './home.js'
+import generateHome from './home.js';
+import generateMenu from './menu.js';
 
 let homeBtn = document.querySelector("#home")
 homeBtn.addEventListener('click', () => generateHome());
+
+let menuBtn = document.querySelector("#menu");
+menuBtn.addEventListener('click', () => generateMenu());
